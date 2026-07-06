864e8656b88de4feb7b08ac22e77f82c3c653dbf
vraic <288215058+vraic@users.noreply.github.com>
2026-07-06 16:21:11 +0100

    #12 update test suite

--- test/system/sessions_test.rb
+++ test/system/sessions_test.rb
@@ -4,6 +4,6 @@ class SessionsTest < ApplicationSystemTestCase
   test "visiting new sessions page" do
     visit new_session_url
 
-    assert_selector "h1", text: "Sign in to your account"
+    assert_selector "h1", text: "Sign in to Vraic"
   end
 end
